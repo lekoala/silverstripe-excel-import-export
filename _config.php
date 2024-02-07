2b3e21ddf6b0060b9b64949700d8a1d8c5231f57
Thomas <thomas@lekoala.be>
2024-02-07 16:26:02 +0100

    remove default yml value

--- _config.php
+++ _config.php
@@ -3,5 +3,9 @@
 use LeKoala\SpreadCompat\SpreadCompat;
 use LeKoala\ExcelImportExport\ExcelImportExport;
 
-SpreadCompat::$preferredCsvAdapter = ExcelImportExport::config()->preferred_csv_adapter ?? 'Native';
-SpreadCompat::$preferredXslxAdapter = ExcelImportExport::config()->preferred_xlsx_adapter ?? 'Native';
+if (ExcelImportExport::config()->preferred_csv_adapter) {
+    SpreadCompat::$preferredCsvAdapter = ExcelImportExport::config()->preferred_csv_adapter;
+}
+if (ExcelImportExport::config()->preferred_xlsx_adapter) {
+    SpreadCompat::$preferredXslxAdapter = ExcelImportExport::config()->preferred_xlsx_adapter;
+}
