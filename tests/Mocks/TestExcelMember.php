4a55f19151cc48cd81ab7137c234cf6e6dcda5fe
Thomas <thomas@lekoala.be>
2025-06-16 13:58:41 +0200

    throw exception in tests for older versions

--- tests/Mocks/TestExcelMember.php
+++ tests/Mocks/TestExcelMember.php
@@ -4,8 +4,17 @@ namespace LeKoala\ExcelImportExport\Test\Mocks;
 
 use SilverStripe\Dev\TestOnly;
 use SilverStripe\Security\Member;
+use Exception;
 
 class TestExcelMember extends Member implements TestOnly
 {
-    // Empty
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+        // For older ss versions that do not validate emails
+        if ($this->Email && !filter_var($this->Email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Email is not valid");
+        }
+    }
 }
